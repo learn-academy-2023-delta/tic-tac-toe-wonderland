0761cd77b2ed62c87a4797b987bd107386b99cd3
miguelosvaldo <miguel.o.molina@icloud.com>
2023-05-30 14:37:13 -0700

    passing it back, delete what i have if need be

--- src/App.js
+++ src/App.js
@@ -1,35 +1,57 @@
-import React, { useState } from 'react'
-import Square from './components/Square'
+import React, { useState, useEffect } from 'react'
+import cell from './components/cell'
 import './App.css'
 
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", "",])
+  const [go, setGo] = useState("circle")
+  const [winningMessage, setWinningMessage] = useState(null)
+  
+  const message = "It is now " + go + " 's go."
 
-const handleGamePlay = (index) => {
-let updatedBoard = [...squares]
-updatedBoard [index] = "❌"
-setSquares(updatedBoard)
-}
+  const checkscore = () => {
+    const winningCombos = [
+      [0,1,2], [3,4,5], [6,7,8],
+      [0,3,6], [1,4,7], [2,5,8],
+      [0,4,8], [2,4,6]
+    ]
+    winningCombos.forEach(array => {
+      let circleWins = array.every(cell => cells[cell] === "circle")
+        if(circleWins) {
+        setWinningMessage("Circle Wins!")
+          return
+        }
+    } )
+  
+  }
+  
+  useEffect(() => {
+    checkscore ()
+  }, [cells])
 
-
-
-
-  return (
+return (
     <>
       <h1>Tic Tac Toe</h1>
-      <div className='gameboard'>
-        {squares.map((value, index) => {
+      <div className="app">
+        <div className="gameboard">
+        {cells.map((cell, index) => {
           return (
-            <Square
+            <Cell
               key = {index}
-              value = {value}
-              index = {index}
-              handleGamePlay = {handleGamePlay}
+              id = {index}
+              cell = {cell}
+              cells = {cells}
+              setCells = {setCells}
+              go = {go}
+              setGo = {setGo}
              />
               
           )
         })}
+        </div>
+        
            </div>
+           <p>{winningMessage || message}</p>
       
     </>
       )
