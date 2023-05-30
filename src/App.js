c1310607fb190e69ee260cc5b8c0c5312bb8c1f0
Rashadjaxon <amirjacksonscholar@gmail.com>
2023-05-30 11:01:59 -0700

    i hand it over to you

--- src/App.js
+++ src/App.js
@@ -5,12 +5,38 @@ import './App.css'
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
 
+const handleGamePlay = (index) => {
+let updatedBoard = [...squares]
+updatedBoard [index] = "❌"
+setSquares(updatedBoard)
+}
+
+
+
+
   return (
     <>
       <h1>Tic Tac Toe</h1>
-      <Square />
+      <div className='gameboard'>
+        {squares.map((value, index) => {
+          return (
+            <Square
+              key = {index}
+              value = {value}
+              index = {index}
+              handleGamePlay = {handleGamePlay}
+             />
+              
+          )
+        })}
+           </div>
+      
     </>
-  )
+      )
 }
 
-export default App
+export default App   
+ 
+
+     
+ 
